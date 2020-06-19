8ef8df47fec0b886216d6150d360b535c7c12113
Richard Clemonsuser.email <rguillermoclemons@gmail.com>
2020-06-19 16:39:38 -0400

    m

--- hoopscript/src/App.js
+++ hoopscript/src/App.js
@@ -5,20 +5,8 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h2>Hoopscript</h2>
+     <p>It's a honor for us to share our POV with the rest of you. Enjoy!</p>
     </div>
   );
 }
